aabd19788ff1a2a823bda00c992f986bf2017096
Vikas Tiwari <91219760+sor-cerer@users.noreply.github.com>
2022-01-18 15:40:56 +0530

    Add files via upload

---
+++ daikoku/sem5/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.db.models import fields
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField()
+    first_name = forms.CharField(max_length=100)
+    last_name = forms.CharField(max_length=100)
+    
+
+    class Meta:
+        model = User
+        fields = [  "first_name" ,"last_name" ,  "email" , "username", "password1", "password2" ]
